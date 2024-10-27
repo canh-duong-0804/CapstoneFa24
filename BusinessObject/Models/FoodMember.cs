c36937745e6f83b8d6332c0d29bdf53ee1a01271
canh-duong-0804 <141054046+canh-duong-0804@users.noreply.github.com>
2024-10-27 18:07:54 +0700

    update class food member

---
+++ BusinessObject/Models/FoodMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class FoodMember
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; } = null!;
+        public string? Portion { get; set; }
+        public double? Calories { get; set; }
+        public double? Protein { get; set; }
+        public double? Carbs { get; set; }
+        public double? Fat { get; set; }
+        public double? VitaminA { get; set; }
+        public double? VitaminC { get; set; }
+        public double? VitaminD { get; set; }
+        public double? VitaminE { get; set; }
+        public double? VitaminB1 { get; set; }
+        public double? VitaminB2 { get; set; }
+        public double? VitaminB3 { get; set; }
+        public string? FoodImage { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public bool? Status { get; set; }
+        public int? DietId { get; set; }
+
+        public virtual Member CreatedByNavigation { get; set; } = null!;
+    }
+}
