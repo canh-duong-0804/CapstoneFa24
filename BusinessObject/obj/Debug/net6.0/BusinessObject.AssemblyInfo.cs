3e8bfb0331882f2409a3588f75a2490558bb15dd
canh-duong-0804 <141054046+canh-duong-0804@users.noreply.github.com>
2024-10-27 14:59:15 +0700

    update

--- BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
+++ BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc115a519db249634b33555e382d48361f1b033")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DataAccess/CommunityCategoryDAO.cs
+++ DataAccess/CommunityCategoryDAO.cs
@@ -1,3 +1,5 @@
+using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,8 +28,105 @@ namespace DataAccess
             }
         }
 
+        public async Task<CommunityPostCategory> CreateCategoryAsync(CommunityPostCategory category)
+        {
+            try
+            {
+                using (var context = new HealthTrackingDBContext())
+                {
 
+                    await context.CommunityPostCategories.AddAsync(category);
+                    await context.SaveChangesAsync();
+                    return category;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error creating category: " + e.Message);
+            }
+        }
 
+        // Read all CommunityPostCategories
+        public async Task<List<CommunityPostCategory>> GetAllCategoriesAsync()
+        {
+            try
+            {
+                using (var context = new HealthTrackingDBContext())
+                {
+                    return await context.CommunityPostCategories.ToListAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error retrieving categories: " + e.Message);
+            }
+        }
 
-	}
+        // Read a single CommunityPostCategory by ID
+        public async Task<CommunityPostCategory> GetCategoryByIdAsync(int id)
+        {
+            try
+            {
+                using (var context = new HealthTrackingDBContext())
+                {
+                    return await context.CommunityPostCategories.FindAsync(id);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error retrieving category: " + e.Message);
+            }
+        }
+
+        // Update an existing CommunityPostCategory
+        public async Task<CommunityPostCategory> UpdateCategoryAsync(CommunityPostCategory category)
+        {
+            try
+            {
+                using (var context = new HealthTrackingDBContext())
+                {
+                    var existingCategory = await context.CommunityPostCategories.FindAsync(category.CommunityCategoryId);
+                    if (existingCategory == null)
+                    {
+                        throw new Exception("Category not found.");
+                    }
+
+                    existingCategory.CommunityCategoryName = category.CommunityCategoryName;
+                    context.CommunityPostCategories.Update(existingCategory);
+                    await context.SaveChangesAsync();
+                    return existingCategory;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error updating category: " + e.Message);
+            }
+        }
+
+        // Delete a CommunityPostCategory by ID
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            try
+            {
+                using (var context = new HealthTrackingDBContext())
+                {
+                    var category = await context.CommunityPostCategories.FindAsync(id);
+                    if (category == null)
+                    {
+                        throw new Exception("Category not found.");
+                    }
+
+                    context.CommunityPostCategories.Remove(category);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error deleting category: " + e.Message);
+            }
+        }
+
+    }
 }
+
